359c05298147019dcf6d979fcadc872a3b7c7480
epsilon64 <benraffin@gmail.com>
2012-02-14 01:30:32 +0100

    dcd

--- GuessEv/app/controllers/keyword_controller.rb
+++
@@ -1,2 +0,0 @@
-class KeywordController < ApplicationController
-end
--- GuessEv/config/routes.rb
+++ GuessEv/config/routes.rb
@@ -50,7 +50,7 @@ GuessEv::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'titles#index'
 
   # See how all your routes lay out with "rake routes"
 
@@ -58,6 +58,6 @@ GuessEv::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  resources :title
+  resources :titles
 
 end
